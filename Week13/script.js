adeda111a5dc47eedd2c387805681a3260170997
@CatherineVas <vasekat8686@gmail.com>
2022-11-19 21:51:02 +0300

    js_first

---
+++ Week13/script.js
@@ -0,0 +1,29 @@
+function clickOnButton() {
+let name = prompt('Как Вас зовут?','Аноним');
+alert(`Привет, ${name}`);
+} 
+
+function calcSum(){
+let a = prompt("Первое число?", 1);
+let b = prompt("Второе число?", 2);
+alert(+a + +b);
+}
+function calcMin(){
+let a = prompt("Первое число?", 1);
+let b = prompt("Второе число?", 2);
+alert(a - b);
+}
+
+function calcDiv(){
+let a = prompt("Первое число?", 1);
+let b = prompt("Второе число?", 2);
+alert(a / b);
+}
+
+function calcMult(){
+let a = prompt("Первое число?", 1);
+let b = prompt("Второе число?", 2);
+alert(a * b);
+}
+
+
